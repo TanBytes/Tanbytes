691283add1bf563d616251582b64bc1646f80f37
Ashok <ashok@tanbytes.com>
2024-07-21 00:53:29 +0530

    chore: Update deploy script for FTP deployment

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,17 @@
-name: FTP Deploy
+#!/bin/bash
 
-on:
-  push:
-    branches:
-      - main
+# FTP Details
+HOST=$HOSTINGER_FTP_HOST
+USER=$HOSTINGER_FTP_USERNAME
+PASS=$HOSTINGER_FTP_PASSWORD
 
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
+# Local and Remote Directories
+LOCAL_DIR="."
+REMOTE_DIR="/public_html/"
 
-    steps:
-    - name: Checkout code
-      uses: actions/checkout@v2
-
-    - name: Install lftp
-      run: sudo apt-get update && sudo apt-get install -y lftp
-
-    - name: Deploy to FTP
-      env:
-        HOSTINGER_FTP_HOST: ${{ secrets.HOSTINGER_FTP_HOST }}
-        HOSTINGER_FTP_USERNAME: ${{ secrets.HOSTINGER_FTP_USERNAME }}
-        HOSTINGER_FTP_PASSWORD: ${{ secrets.HOSTINGER_FTP_PASSWORD }}
-      run: |
-        #!/bin/bash
-        HOST=$HOSTINGER_FTP_HOST
-        USER=$HOSTINGER_FTP_USERNAME
-        PASS=$HOSTINGER_FTP_PASSWORD
-        LOCAL_DIR="."
-        REMOTE_DIR="/public_html/"
-
-        lftp -u $USER,$PASS $HOST <<EOF
-        set ssl:verify-certificate no
-        mirror --reverse --delete --verbose --exclude-glob .git* $LOCAL_DIR $REMOTE_DIR
-        bye
-        EOF
+# Upload Files
+lftp -u $USER,$PASS $HOST <<EOF
+set ssl:verify-certificate no
+mirror --reverse --delete --verbose --exclude-glob .git* $LOCAL_DIR $REMOTE_DIR
+bye
+"
