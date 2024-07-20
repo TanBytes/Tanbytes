4b57fd69c3759a57110e0cfc0d532a5223e5959b
Ashok <s.ashok.kumar@siemensgamesa.com>
2024-07-20 17:01:56 +0530

    First Commit

---
+++ pay/js/loadData.js
@@ -0,0 +1,62 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleButton');
+    var collapsibleContent = document.querySelector('.collapsible-content');
+
+    toggleButton.addEventListener('click', function () {
+        if (collapsibleContent.classList.contains('open')) {
+            collapsibleContent.classList.remove('open');
+            toggleButton.textContent = 'See More';
+        } else {
+            collapsibleContent.classList.add('open');
+            toggleButton.textContent = 'See Less';
+        }
+    });
+});
+
+function socialScript() {
+    function openShare(e) {
+        try {
+            let elem = e.target;
+            if (elem.tagName == "A") {
+                elem = elem.firstElementChild;
+            }
+            const webaddress = window.location.href;
+            let gourl = elem.dataset.gourl;
+            let openurl = gourl + webaddress;
+            if (gourl) {
+                window.open(openurl, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+    let socialShareButtonsItem = document.querySelectorAll(".courses__details-social a");
+    socialShareButtonsItem.forEach(element => {
+        element.onclick = openShare;
+    });
+};
+function printmsgtopend(type, msg, btn, time) {
+    Swal.fire({
+        position: 'top-end',
+        icon: type,
+        title: msg,
+        allowOutsideClick: false,
+        showConfirmButton: btn,
+        timer: time
+    })
+}
+function printmsgcenter(type, msg, btn, time) {
+    Swal.fire({
+        position: 'center',
+        icon: type,
+        title: msg,
+        allowOutsideClick: false,
+        showConfirmButton: btn,
+        timer: time
+    })
+}
+function closemsg() {
+    swal.close();
+}
+socialScript();
---
+++ pay/js/pay.js
@@ -0,0 +1,31 @@
+﻿function payinitiate(btn) {
+    var cid = $(btn).attr("data-id");
+
+    // Check session before proceeding
+    $.ajax({
+        type: "POST",
+        url: "../Course.aspx/payforCoursePurchase",
+        data: "{'cid':'" + cid + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (r) {
+            if (r != null) {
+                swal.close();
+                if (r.d.status == "2") {
+                    printmsgcenter('warning', r.d.msg, false, 2000);
+                    setTimeout(function () { window.location.href = '../login.aspx'; }, 2000);
+                }
+                if (r.d.status == "1") {
+                    printmsgcenter('success', r.d.msg, false, 2000);
+                    window.open(r.d.url, '_self');
+                }
+                if (r.d.status == "0") {
+                    printmsgcenter('warning', r.d.msg, false, 3000);
+                }
+            }
+        },
+        error: function () {
+            printmsgcenter('error', 'Unable to verify session. Please try again.', true, 5000);
+        }
+    });
+}
